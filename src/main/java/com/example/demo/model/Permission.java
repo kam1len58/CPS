75ea4e7f159228cf88445457e1d63f6fe0e42dbe
Kamil <kamil.228.garipov@gmail.com>
2025-09-16 23:24:36 +0300

    Revert "develop: controller and models added"
    This reverts commit 4ab15841aca7be02adf39fab359493fec7060d80.
    
    # Conflicts:
    #	.gitignore

--- src/main/java/com/example/demo/model/Permission.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.model;
-
-public class Permission {
-    private Long id;
-    private String title;
-}
--- src/main/java/com/example/demo/model/RiskLevel.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.model;
-
-public enum RiskLevel {
-    LOW,
-    MEDIUM,
-    HIGH
-}
